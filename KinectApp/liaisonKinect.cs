9893db74f010e9b7acc2dbb19fb2e91b6610dc3e
tomiboul <115427147+tomiboul@users.noreply.github.com>
2025-10-22 14:41:23 +0200

    struture projet

---
+++ KinectApp/liaisonKinect.cs
@@ -0,0 +1,9 @@
+using System;
+ // dans le terminal -> etre via 'cd' dans le fichier -> executer "   dotnet run  "
+class liaisonKinect
+{
+    static void Main()
+    {
+        Console.WriteLine("coucou");
+    }
+}
