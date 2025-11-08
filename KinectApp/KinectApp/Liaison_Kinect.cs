8aa6424f0d715e2b3215b3d3e1254094371cdf28
LouisBomal <louis.bomal@student.unamur.be>
2025-11-09 00:08:33 +0100

    Son fonctionnel, fluidité moyenne
    Son fonctionnel pour la distance entre les mains
    Fluidité moyenne ajoutée, pourrait surement être mieux
    Test en avance des paramètres optimaux pour le volume et la fréquence, à tester ensemble par la suite

--- KinectApp/KinectApp/Liaison_Kinect.cs
+++ KinectApp/KinectApp/Liaison_Kinect.cs
@@ -3,19 +3,35 @@ using System.Data;
 using System.Linq;
 using Microsoft.Kinect;
 using KinectApp;
+using NAudio.Wave;
+using System.Windows.Controls;
 
 namespace KinectHeadPositionConsole
 {
     class Program
     {
-        private static float treshold 
+        private static float treshold;
         private static KinectSensor sensor = null;
         private static Body[] bodies = null;
         private static BodyFrameReader bodyFrameReader = null;
+        private static WaveOut waveOut = null;
+        private static SoundProvider soundProvider;
+        private static int frame = 0;
+        private static DateTime lastFrame = DateTime.Now;
+
+        //framerate of the kinect device, frequency and volume used for pitch interpolation
+        private const int frameRate = 30;
+        private static float lastFreq;
+        private static float currentFreq;
+        private static float lastVol;
+        private static float currentVol;
+        private static float previous_distance;
+
+        private static float loopFrame;
 
         static void Main(string[] args)
         {
-            
+                
                 //initializes the sensor aand the frame reader
                 sensor = KinectSensor.GetDefault();
 
@@ -32,14 +48,20 @@ namespace KinectHeadPositionConsole
                     if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape)
                         break;
 
+                
+                    loopFrame++;
+
                     System.Threading.Thread.Sleep(50);
                 }
 
 
                 //frees the resources
+
                 bodyFrameReader.FrameArrived -= BodyFrameReader_FrameArrived;
                 bodyFrameReader.Dispose();
                 sensor.Close();
+                StopSineWave();
+               
 
                 Console.WriteLine("Stopped");
 
@@ -51,6 +73,9 @@ namespace KinectHeadPositionConsole
         {
             using (BodyFrame f = e.FrameReference.AcquireFrame())//gets the current frame 
             {
+                //Console.WriteLine("frame count : " + frame);
+                frame++;
+                
                 if (f != null)
                 {
                     if (bodies == null)
@@ -59,7 +84,14 @@ namespace KinectHeadPositionConsole
                     }
                     else {
 
-                            f.GetAndRefreshBodyData(bodies);
+                        f.GetAndRefreshBodyData(bodies);
+
+                        //init the soundprovider instance
+                        
+                        if (waveOut == null)
+                        {
+                            StartSineWave();   
+                        }
 
                         //gets the body tracked by the kinect device
                         Body trackedBody = null;
@@ -76,13 +108,79 @@ namespace KinectHeadPositionConsole
                             CameraSpacePoint position_hand_right = right_hand_j.Position;
                             Joint left_hand_j = trackedBody.Joints[JointType.HandLeft];
                             CameraSpacePoint position_hand_left = left_hand_j.Position;
-
+                            
                             float distance = utility.compute_distance(position_hand_right.X, position_hand_right.Y, position_hand_left.X, position_hand_left.Y);
-                            Console.WriteLine(distance);
+
+
+                            lastFreq = currentFreq;
+                            lastVol = currentVol;
+                            //can be changed if leads to bad audios
+
+                            //currentVol = (float)(Math.Cos(6*distance+Math.PI)+1)/2;
+                            currentVol = 0.4f;
+                            currentFreq = 300 / distance;
+                            
+                            
+                            if (Math.Abs(previous_distance - distance) <= 0.01)
+                            {
+                                currentVol = 0;
+                            }
+                            previous_distance = distance;
+
+                            if (currentFreq <= 20) {
+                                currentFreq = 20;
+                            }
+                            if (currentFreq >= 800) {
+                                currentFreq = 800;
+                            }
+
+
+                            //Console.WriteLine("updated normally");
+                            UpdateSineWave(currentVol, currentFreq);
+                            
+                            //Console.WriteLine(distance);
                          
                         }                                              
                     }
                 }
+                lastFrame = DateTime.Now;
+                //Console.WriteLine("frame finsihed");
+            }
+        }
+
+        private static void StartSineWave()
+        {
+            if (waveOut == null)
+            {
+            
+                soundProvider = new SoundProvider();
+                soundProvider.SetWaveFormat(16000, 1); // 16kHz mono
+                soundProvider.Frequency = 500;
+                soundProvider.Volume = 0;
+                lastFreq = 1000;
+                lastVol = (float)0.4;
+                waveOut = new WaveOut();
+                waveOut.DesiredLatency = 120;
+                waveOut.Init(soundProvider);
+                waveOut.Play();
+            }
+        }
+
+        private static void StopSineWave() {
+            if (!( waveOut == null))
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+            }
+        }
+
+        private static void UpdateSineWave(float Vol, float Freq)
+        {
+            if (!(soundProvider == null))
+            {
+                soundProvider.targetFreq = Freq;
+                soundProvider.targetVol = Vol;
             }
         }
     }
--- KinectApp/KinectApp/SoundProvider.cs
+++ KinectApp/KinectApp/SoundProvider.cs
@@ -13,21 +13,49 @@ namespace KinectApp
         int sample;
         public float Frequency { get; set; }
         public float Volume { get; set; }
-        
+
+        private double phase;
+
+        //variables to modify the sound in a fluid way
+        public float targetFreq;    
+        public float targetVol;
+        private float coeffFreq = 0.03f;
+        private float coeffVol = 0.03f;
         public SoundProvider() {
             Frequency = 1000;
             Volume = 0.25f;
+            phase = 0;
 
         }
 
         //computes values of samples asked by the sound chip 
-        public override int Read(byte[] buffer, int offset, int count)
+        public override int Read(float[] buffer, int offset, int count)
         {
-            int sampleRate = WaveFormat.SampleRate;
-            for (int i = 0;  i < count; i++) {
-                //y(t) = A⋅sin(2πft)(added in the buffer)
-                buffer(offset + i) = (float)Volume * Math.Sin(2 * Math.PI * Frequency * sample);
-        }
+           
+            double phasediff = 2 * Math.PI * Frequency / WaveFormat.SampleRate;
 
+            //target frequency and volumes to modify the sound in a fluid way, and if the frequency greatly differs from one frame to another, the sound changes faster
+            coeffFreq = Math.Max(0.03f, Math.Abs(targetFreq- Frequency)*0.0005f);
+            Frequency += (targetFreq - Frequency) * coeffFreq;
+            coeffFreq = Math.Max(0.03f, Math.Abs(targetVol - Volume));
+            
+            Volume += (targetVol - Volume) * coeffVol;
+            Console.WriteLine("Changement");
+            Console.WriteLine(targetVol);
+            Console.WriteLine(Volume);
+            if (Volume<= 0.1&& targetVol == 0) { Volume = 0; }
+            if (Volume <= 0.1 && targetVol != 0) { Volume += 0.1f; }
+            for (int i = 0; i < count; i++)
+            {
+                //y(t) = A⋅sin(2πft)(added in the buffer)
+                buffer[offset+i] = (float)(Volume * Math.Sin(phase));
+                phase += phasediff;
+                if (phase > 2 * Math.PI) 
+                {
+                    phase -= 2 * Math.PI; 
+                }
+            }
+            return count;
+        }
     }
 }
