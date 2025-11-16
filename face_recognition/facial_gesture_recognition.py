e149010b2831eda001574ee3953f16ab3506eb1f
Youra73 <houria.ariouat@student.unamur.be>
2025-11-16 16:43:02 +0100

    correction nod (encore à tester)

--- face_recognition/facial_gesture_recognition.py
+++ face_recognition/facial_gesture_recognition.py
@@ -1,23 +1,9 @@
 import mediapipe as mp
 import cv2
-
+import time
 mp_drawing = mp.solutions.drawing_utils
 mp_holistic = mp.solutions.holistic
 mp_face_mesh = mp.solutions.face_mesh
-
-# for the webcam
-
-cap = cv2.VideoCapture(0)
-while cap.isOpened():
-    ret, frame = cap.read()
-    cv2.imshow("Raw Webcam Feed", frame)
-
-    if cv2.waitKey(10) & 0xFF == ord("q"):
-        break
-cap.release()
-cv2.destroyAllWindows()
-cap.release()
-cv2.destroyAllWindows()
 
 
 # Chut gestuel
@@ -38,12 +24,12 @@ def chut(results, hands):
 
             index_tip = hand[8]
             index_base = hand[5]
+            wrist = hand[0]
             mouth_center_x = (face[13].x + face[14].x) / 2
             mouth_center_y = (face[13].y + face[14].y) / 2
-
-            # distance for the up finger
-
-            up_index = index_tip.y < index_base.y
+            
+            #distance for the up finger
+            up_index = (index_tip.y < index_base.y < wrist.y) and (abs(index_tip.x - index_base.x) < 0.05)
 
             # compute the distance between two points with pythagore
 
@@ -74,12 +60,12 @@ def raised_finger(hands):
 
             index_tip = hand[8]
             index_base = hand[5]
+            wrist = hand[0]
             mouth_center_x = (face[13].x + face[14].x) / 2
             mouth_center_y = (face[13].y + face[14].y) / 2
-
-            # distance for the up finger
-
-            up_index = index_tip.y < index_base.y
+            
+            #distance for the up finger
+            up_index = (index_tip.y < index_base.y < wrist.y) and (abs(index_tip.x - index_base.x) < 0.05)
 
             # compute the distance between two points with pythagore
 
@@ -94,7 +80,10 @@ def raised_finger(hands):
 
 chin_initial_y = None
 number_of_nod = 0
-
+head_down = False
+head_up = False
+current_time = None
+last_time = None
 
 def nod_head(results):
 
@@ -102,34 +91,63 @@ def nod_head(results):
 
     global chin_initial_y
     global number_of_nod
+    global head_down
+    global head_up 
+    global current_time
+    global last_time
+
 
     if results.face_landmarks:
 
         # chin
-
         chin_center = results.face_landmarks.landmark[152]
-        # forehead
-
-        forehead_center = results.face_landmarks.landmark[10]
-        # if the position of the chin doesnt change we left the function
-
+        #if the position of the chin doesnt change we left the function
         if chin_initial_y == None:
+            current_time = last_time
             chin_initial_y = chin_center.y
             return False
-        # if there is a certain number of nod then we have the nodding
+        
+        #distance to detecte a head up
+        if head_up == False and chin_center.y - chin_initial_y < -0.025 :
+            print("HEAD UP")
+            current_time = time.time() 
+            head_up = True
 
-        if chin_center.y - chin_initial_y > 0.02:
-            number_of_nod += 1
-            if number_of_nod >= 4:
 
-                print("Oui de la tete")
-                # reset de number of nod after every nod
+        
+        #distance to detect a head down
+        if head_down == False and head_up == True and abs(chin_center.y - chin_initial_y) < 0.015: 
+            print("HEAD DOWN")
+            last_time = time.time()
+            head_down = True
+            head_up = False
 
+
+            if current_time and last_time : 
+                if last_time - current_time <= 2 :
+                    number_of_nod += 1
+                else : 
+                    number_of_nod = 0
+            
+            if number_of_nod >= 1 :
+                print("Oui de la tete")              
+                #reset de number of nod after every nod
+                head_down = False
+                head_up = False
                 number_of_nod = 0
+                last_time = None
+                current_time = None
                 return True
+            head_down = False
+            head_up = False
+        
+
+            
 
 
-# for the hand, face and body detection
+       
+            
+
 
 cap = cv2.VideoCapture(0)
 # Initiate holistic model
@@ -140,10 +158,12 @@ with mp_holistic.Holistic(
 
     while cap.isOpened():
         ret, frame = cap.read()
+        frame_for_mp = frame.copy()
 
+        frame_display = cv2.flip(frame, 1)
+        
         # Recolor Feed
-
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = cv2.cvtColor(frame_display, cv2.COLOR_BGR2RGB)
         # Make Detections
 
         results = holistic.process(image)
@@ -198,80 +218,3 @@ cap.release()
 cv2.destroyAllWindows()
 
 mp_holistic.POSE_CONNECTIONS
-
-
-# styling
-
-mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=2)
-mp_drawing.draw_landmarks
-
-cap = cv2.VideoCapture(0)
-# Initiate holistic model
-
-with mp_holistic.Holistic(
-    min_detection_confidence=0.5, min_tracking_confidence=0.5
-) as holistic:
-
-    while cap.isOpened():
-        ret, frame = cap.read()
-
-        # Recolor Feed
-
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # Make Detections
-
-        results = holistic.process(image)
-        # print(results.face_landmarks)
-
-        # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
-
-        # Recolor image back to BGR for rendering
-
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-        # 1. Draw face landmarks
-
-        mp_drawing.draw_landmarks(
-            image,
-            results.face_landmarks,
-            mp_face_mesh.FACEMESH_CONTOURS,
-            mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
-            mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1),
-        )
-
-        # 2. Right hand
-
-        mp_drawing.draw_landmarks(
-            image,
-            results.right_hand_landmarks,
-            mp_holistic.HAND_CONNECTIONS,
-            mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
-            mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2),
-        )
-
-        # 3. Left Hand
-
-        mp_drawing.draw_landmarks(
-            image,
-            results.left_hand_landmarks,
-            mp_holistic.HAND_CONNECTIONS,
-            mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
-            mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2),
-        )
-
-        # 4. Pose Detections
-
-        mp_drawing.draw_landmarks(
-            image,
-            results.pose_landmarks,
-            mp_holistic.POSE_CONNECTIONS,
-            mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
-            mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2),
-        )
-
-        cv2.imshow("Raw Webcam Feed", image)
-
-        if cv2.waitKey(10) & 0xFF == ord("q"):
-            break
-cap.release()
-cv2.destroyAllWindows()
